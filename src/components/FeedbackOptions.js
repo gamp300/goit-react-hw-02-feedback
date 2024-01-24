b971ec9a634ef511e038774b09222c89630b4f80
gamp300 <49374048+gamp300@users.noreply.github.com>
2024-01-23 20:04:47 -0500

    React Feedback One

---
+++ src/components/FeedbackOptions.js
@@ -0,0 +1,15 @@
+import { FeedbackButton } from './Feedback.Styled';
+
+const FeedbackOptions = ({ options, onLeaveFeedback }) => {
+  return (
+    <div>
+      {options.map(option => (
+        <FeedbackButton key={option} onClick={() => onLeaveFeedback(option)}>
+          {option}
+        </FeedbackButton>
+      ))}
+    </div>
+  );
+};
+
+export default FeedbackOptions;
---
+++ src/components/Section.js
@@ -0,0 +1,14 @@
+import { FeedbackContainer } from './Feedback.Styled';
+
+const Section = ({ title, children }) => {
+  return (
+    <div>
+      <FeedbackContainer>
+        <h2>{title}</h2>
+        {children}
+      </FeedbackContainer>
+    </div>
+  );
+};
+
+export default Section;
